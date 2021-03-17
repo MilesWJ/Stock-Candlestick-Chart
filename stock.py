03530e8098e6941cf58f53c200bdb84393223f8e
Miles James <milesw.james@gmail.com>
2021-03-17 14:17:40 -0700

    Delete stock.py

--- stock.py
+++
@@ -1,7 +0,0 @@
-class Stock(object):
-    def __init__(self, stockticker, finance_api, start, end):
-        self.stockticker = stockticker
-        self.finance_api = finance_api
-        self.start = start
-        self.end = end
-
