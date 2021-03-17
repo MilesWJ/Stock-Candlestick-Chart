3ca2279bee6a6f7a234d6f3d745f8ed98b072bf0
Miles James <milesw.james@gmail.com>
2021-03-17 14:17:58 -0700

    Add files via upload

---
+++ Stock Candlestick Chart/stock.py
@@ -0,0 +1,6 @@
+class Stock(object):
+    def __init__(self, stockticker, finance_api, start, end):
+        self.stockticker = stockticker
+        self.finance_api = finance_api
+        self.start = start
+        self.end = end
